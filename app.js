8dc49b99423a022227e849caaef9d51c7e92cb02
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-09-23 16:08:09 -0400

    delete previous code from app js file

--- app.js
+++ app.js
@@ -6,14 +6,3 @@ const router = express.Router()
 app.listen(3000, function(){
     console.log("Listening on port 3000")
 });
-
-// make api using routes (to handle browser requests)
-//GET request 
-// app.get("/hello", function(req, res){
-//    res.send("<h1>Hello Express</h1>")
-// });
-
-// app.get("/goodbye", function(req, res){
-//    res.send("<h1>Goodbye Express</h1>")
-// });
-
