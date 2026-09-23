6beb8cf22a761f052935df30df96d88f245a18eb
Kira EB <107140669+KemmettBuck@users.noreply.github.com>
2026-09-23 18:52:53 -0400

    add new songs in a const variable

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const app = express()
 const router = express.Router()
 
 router.get("/songs", function(req, res){
-    const song = {
-        title: "Uptown Funk",
-        artist: "Bruno Mars",
-        popularity: 10,
-        genre: ["funk", "boogie"]
-    }
 
-    res.json(song)
-});
+const songs = [
+            {
+                title: "We Found Love",
+                artist: "Rihanna",
+                popularity: 10,
+                releaseDate: new Date(2011, 9, 22),
+                genre: ["electro house"]
+            },
+            {
+                title: "Happy",
+                artist: "Pharrell Williams",
+                popularity: 10,
+                releaseDate: new Date(2013, 11, 21),
+                genre: ["soul", "new soul"]
+            }
+        ];
+
+    res.json(songs)
+    })
 
 // all requests usually using an api start with /api.. so url would be localhost:3000/api/songs
 app.use("/api", router)
